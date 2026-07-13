fc0f4103e74d4a7f3b8b7e8f831b791a246ea292
yurickn <yurick11@gmail.com>
2026-07-13 23:09:29 +0200

    CI config fix to test; self test name fix

--- test/self_test.cpp
+++ test/self_test.cpp
@@ -1,12 +1,14 @@
 //
 // Created by yurickn on 11/07/2026.
 //
+// The simplest test case checking testing is working; example of test
+//
 
 #include <istream>
 
 #include "gtest/gtest.h"
 
-TEST(Test, test1) {
+TEST(SelfTestSuit, self_test) {
     std::cout << "Hello World :) LOL" << std::endl;
     EXPECT_EQ(1, 1);
 }
