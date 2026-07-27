f2382ea02404db1aaf0f709e083b26e34762b98c
yurickn <yurick11@gmail.com>
2026-07-27 22:14:49 +0200

    GeneratorTests.cpp refactored to a parameterized test

--- test/GeneratorTests.cpp
+++ test/GeneratorTests.cpp
@@ -5,79 +5,82 @@
 #include "gtest/gtest.h"
 #include "../src/Generator.cpp"
 
-TEST(GeneratorTests, shouldRepeatValues)
+namespace
+{
+    class GeneratorTests: public testing::TestWithParam<
+                std::tuple<
+                    std::string,
+                    std::function<void (int)>,
+                    std::function<int (int, int)>
+                >
+            > {};
+
+    Generator<int> *generator = nullptr;
+}
+
+TEST_P(GeneratorTests, sholdBeValueChangedByDifferentGeneratorParameters)
 {
     // Given
     constexpr auto expected_count = 100;
-    const auto generator = Generator<int>();
+    constexpr auto changer = 5;
+    generator = new Generator<int>(); //TODO: move to before_each
+    const auto set = std::get<1>(GetParam());
+    set(changer);
 
     // When
     auto actual_angle_to_value_map = std::map<int, int>();
     for (auto i = 0; i < expected_count; i++)
     {
-        actual_angle_to_value_map[i] = generator[i];
+        actual_angle_to_value_map[i] = (*generator)[i];
     }
 
     // Then
     ASSERT_EQ(expected_count, actual_angle_to_value_map.size());
 
+    const auto change_expected_value = std::get<2>(GetParam());
     std::for_each(
         actual_angle_to_value_map.begin(), actual_angle_to_value_map.end(),
-        [](auto const &pair)
+        [&change_expected_value](auto const &pair)
         {
-            ASSERT_EQ(pair.first, pair.second);
+            ASSERT_EQ(change_expected_value(pair.first, changer), pair.second);
         }
     );
+    free(generator); //TODO: move to tear_down
 }
 
-TEST(GeneratorTests, shouldRepeatValuesWithMultipliedFrequency)
-{
-    // Given
-    constexpr auto expected_count = 100;
-    auto generator = Generator<int>();
-    generator.setFrequency(5);
-
-    // When
-    auto actual_angle_to_value_map = std::map<int, int>();
-    for (auto i = 0; i < expected_count; i++)
-    {
-        actual_angle_to_value_map[i] = generator[i];
-    }
-
-    // Then
-    ASSERT_EQ(expected_count, actual_angle_to_value_map.size());
-
-    std::for_each(
-        actual_angle_to_value_map.begin(), actual_angle_to_value_map.end(),
-        [](auto const &pair)
-        {
-            ASSERT_EQ(pair.first * 5, pair.second);
-        }
-    );
-}
-
-TEST(GeneratorTests, shouldRepeatValuesWithAddedPhase)
-{
-    // Given
-    constexpr auto expected_count = 100;
-    auto generator = Generator<int>();
-    generator.setPhase(5);
-
-    // When
-    auto actual_angle_to_value_map = std::map<int, int>();
-    for (auto i = 0; i < expected_count; i++)
-    {
-        actual_angle_to_value_map[i] = generator[i];
-    }
-
-    // Then
-    ASSERT_EQ(expected_count, actual_angle_to_value_map.size());
-
-    std::for_each(
-        actual_angle_to_value_map.begin(), actual_angle_to_value_map.end(),
-        [](auto const &pair)
-        {
-            ASSERT_EQ(pair.first + 5, pair.second);
-        }
-    );
-}
+INSTANTIATE_TEST_SUITE_P(
+    GeneratorTests,
+    GeneratorTests,
+    ::testing::Values(
+        std::make_tuple(
+            "shouldRepeatValues",
+            [&]([[maybe_unused]]const int value_to_set){},
+            [](const int arg, [[maybe_unused]]const int changer)
+            {
+            return arg;
+            }
+        ),
+        std::make_tuple(
+            "shouldRepeatValuesWithMultipliedFrequency",
+            [&](const int value_to_set)
+            {
+            generator->setFrequency(value_to_set);
+            },
+            [](const int arg, const int changer)
+            {
+            return arg * changer;
+            }
+        ),
+        std::make_tuple(
+            "shouldRepeatValuesWithAddedPhase",
+            [&](const int value_to_set)
+            {
+            generator->setPhase(value_to_set);
+            },
+            [](const int arg, const int changer)
+            {
+            return arg + changer;
+            }
+        )
+    )
+);
